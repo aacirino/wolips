1e2dae629dea6a3b4eb4f528c16853be0d7ea3c8
Ângelo Andrade Cirino <aacirino@visionar.com.br>
2015-05-08 12:26:09 -0300

    Improvements to the Migration0 template.

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/templates/Migration0.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/templates/Migration0.java
@@ -1,3 +1,14 @@
+package ${model.name}
+
+#foreach ($entity in $model.entities)
+#if ($model.userInfo.packageImport)
+#if ($entity.classNameWithoutPackage != "EOGenericRecord" and $entity.classNameWithoutPackage != "ERXGenericRecord")
+import ${entity.classNameWithoutPackage}.*;
+#end
+#end
+#end
+
+import com.webobjects.eoaccess.EOModel;
 import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.foundation.NSArray;
 
@@ -6,8 +17,9 @@ import er.extensions.migration.ERXMigrationDatabase;
 import er.extensions.migration.ERXMigrationIndex;
 import er.extensions.migration.ERXMigrationTable;
 import er.extensions.migration.ERXModelVersion;
+import er.extensions.migration.IERXPostMigration;
 
-public class ${model.name}0 extends ERXMigrationDatabase.Migration {
+public class ${model.name}0 extends ERXMigrationDatabase.Migration implements IERXPostMigration {
 	@Override
 	public NSArray<ERXModelVersion> modelDependencies() {
 		return null;
@@ -23,5 +35,16 @@ public class ${model.name}0 extends ERXMigrationDatabase.Migration {
 #foreach ($entityMigration in $entityMigrations)${entityMigration}
 #end
 #foreach ($foreignKeyMigration in $foreignKeyMigrations)${foreignKeyMigration}#end
+	}
+
+	public void postUpgrade(EOEditingContext editingContext, EOModel model) throws Throwable {
+		// INSERT YOUR POST UPGRADE CODE HERE
+		
+		// Initial data for each of the entities in this model
+#foreach ($entity in $model.entities)
+#if ($entity.classNameWithoutPackage != "EOGenericRecord" and $entity.classNameWithoutPackage != "ERXGenericRecord")
+		${entity.classNameWithoutPackage}.populateInitialData(editingContext);
+#end
+#end
 	}
 }
